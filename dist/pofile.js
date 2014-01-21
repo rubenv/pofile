4fa1ce75c563dc69ed3de7b3413e04959648d82c
Ruben Vermeersch <ruben@rocketeer.be>
2014-01-21 16:01:08 +0100

    Recompile.

--- dist/pofile.js
+++ dist/pofile.js
@@ -1,4 +1,4 @@
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"+dPpep":[function(require,module,exports){
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"W8CkM0":[function(require,module,exports){
 var fs = require('fs'),
     isArray = require('lodash.isarray');
 
@@ -225,7 +225,7 @@ PO.Item.prototype.toString = function () {
 module.exports = PO;
 
 },{"fs":3,"lodash.isarray":4}],"pofile":[function(require,module,exports){
-module.exports=require('+dPpep');
+module.exports=require('W8CkM0');
 },{}],3:[function(require,module,exports){
 
 },{}],4:[function(require,module,exports){
@@ -311,4 +311,4 @@ function isNative(value) {
 
 module.exports = isNative;
 
-},{}]},{},["+dPpep"])
+},{}]},{},["W8CkM0"])
